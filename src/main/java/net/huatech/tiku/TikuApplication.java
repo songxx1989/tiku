95f89112047a29be7370cca577fbaee4a66d0d58
song <songxx1989@foxmail.com>
2017-11-08 15:38:02 +0800

    init spring boot project

---
+++ src/main/java/net/huatech/tiku/TikuApplication.java
@@ -0,0 +1,12 @@
+package net.huatech.tiku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikuApplication.class, args);
+	}
+}
